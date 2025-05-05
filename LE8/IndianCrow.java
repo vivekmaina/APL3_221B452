e1a69ec67c0787bb567b882c3a811bc9c1f71f08
vivekmaina <116014373+vivekmaina@users.noreply.github.com>
2025-05-05 11:49:59 +0530

    Add files via upload

---
+++ LE8/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("Cawing");
+    }
+    public void fly(){
+        System.out.println("Flying over mountain");
+    }
+    public void eat(){
+        System.out.println("eat");
+    }
+}
---
+++ LE8/Main (1).java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    Crow crow = new IndianCrow();
+	    CrowAdapter crowadapter = new CrowAdapter(crow);
+	    client(crowadapter);
+	}
+	public static void client(Swan swan){
+	    swan.eat();
+	    swan.swim();
+	    swan.sing();
+	}
+}
